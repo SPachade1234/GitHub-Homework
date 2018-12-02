7d0bca4d3fcc99886bbe4b9b33d8fe42aadd8041
Sampat Pachade <sericpachade@gmail.com>
2018-12-02 10:41:01 -0500

    new ArrayList and method
    The method convertArrayList is created, which takes in an int as a parameter. New ArrayList are created: one in the main method and another in the convertArrayList method.

--- GHHW.java
+++ GHHW.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class GHHW{
 
      public static void main(String[] args){
-     
+		ArrayList<Integer> list = new ArrayList<>();
      }
+
+     public static ArrayList<Integer> convertArrayList(int num){
+		ArrayList<Integer> list = new ArrayList<>();
+
+		return list;
+	}
 }
