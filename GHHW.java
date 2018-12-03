5bd228b6efe508636693686c3a9451a9e3614a99
Sampat Pachade <sericpachade@gmail.com>
2018-12-02 22:03:08 -0500

    new code
    Previous code was giving errors, so switched out with new code

--- GHHW.java
+++ GHHW.java
@@ -4,20 +4,19 @@ public class GHHW{
 
      public static void main(String[] args){
 		ArrayList<Integer> list = new ArrayList<>();
-		int num = (int) (Math.random()*9998)+1;
+		int num = 8950;//(int) (Math.random()*9998)+1;
 
 		list = convertArrayList(num);
 		System.out.println(list);
+
+		System.out.println(reconstructArrayList(list));
      }
 
      public static ArrayList<Integer> convertArrayList(int num){
 		ArrayList<Integer> list = new ArrayList<>();
+		int num1 = num;
 		int number = 0;
 		int temp = 0;
-
-		temp = list.get(0);
-		list.set(0, list.get(list.size() - 1));
-		list.set(list.size() - 1, temp);
 
 		while(num > 0){
 			number = num % 10;
@@ -25,6 +24,26 @@ public class GHHW{
 			num = num / 10;
 		}
 
+		temp = list.get(0);
+		list.set(0, list.get(list.size() - 1));
+		list.set(list.size() - 1, temp);
+		if(num1 > 1000){
+			temp = list.get(1);
+			list.set(1, list.get(list.size() - 2));
+			list.set(list.size() - 2, temp);
+		}
+
+
 		return list;
+	}
+
+	public static int reconstructArrayList(ArrayList<Integer> list){
+		int num = 0;
+		num = list.get(0) * 1000;
+		num += list.get(1) * 100;
+		num += list.get(2) * 10;
+		num += list.get(3);
+
+		return num;
 	}
 }
